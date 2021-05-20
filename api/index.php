21229774c7258dbc6c1b60925fa17de6eed2827f
zhullyb <zhullyb@outlook.com>
2021-05-21 00:20:46 +0800

    Sync 20210521-0020

--- api/index.php
+++ api/index.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html> 
-<html> 
-<body> 
-
 <?php
 //获取当前绝对路径
 $path = dirname(__FILE__);
 //读取文本文件
-$file = file($path."/1.txt");
+$file = file($path."/img.txt");
 //随机选择一行作为url
 $line = mt_rand(0, count($file)-1);
 $url = trim($file[$line]);
-
+//303跳转
 if (isset($url))
 {
 Header("HTTP/1.1 303 See Other");
@@ -18,7 +14,3 @@ Header("Location: $url");
 exit;
 }
 ?>
-
-</body> 
-</html>
-
